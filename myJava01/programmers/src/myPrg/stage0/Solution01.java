b6e97579a645e1073f0e5558d7b8eb1c66266688
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-03-19 00:40:39 +0900

    프로그래머스 12937번 문제
    짝수와 홀수

---
+++ myJava01/programmers/src/myPrg/stage0/Solution01.java
@@ -0,0 +1,11 @@
+package programmers.stage1;
+
+public class Solution01 {
+	public String solution(int num) {
+		String answer = "";
+		if (num % 2 == 0) {
+			return answer = "Even";
+		}
+		return answer = "Odd";
+	}
+}
