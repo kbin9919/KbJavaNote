025654c0378b2016371434b9e5ddd53ce4a693a3
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-03-19 09:44:43 +0900

    프로그래머스 12931번 문제
    자릿수 더하기

--- myJava01/programmers/src/myPrg/stage0/Solution.java
+++ myJava01/programmers/src/myPrg/stage0/Solution.java
@@ -2,19 +2,21 @@ package chap0;
 
 public class Solution {
 	
-	public double solution(int[] arr) {
-        double answer = 0;
-        for(int i = 0; i<arr.length; i++) {
-        	answer += arr[i];
+	public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n); //"123"
+        int[] x = new int[str.length()]; //길이가 문자열의 길이와 같음 : x[3]
+        String[] s = new String[str.length()]; //길이가 문자열의 길이와 같음 : s[3]
+        for(int i = 0; i <str.length(); i++) { //0부터 2까지
+        	s[i] = String.valueOf(str.charAt(i)); 
+        	x[i] = Integer.parseInt(s[i]); //x[0] = 1, x[1] = 2, x[2] = 3
+        	System.out.println(x[i]);
+        	answer += x[i]; //0 + 1 + 2 + 3;
         }
-        answer /= arr.length;
-        
         return answer;
-        
     }
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		int[] ss = {1, 3, 3 ,2};
-		System.out.println(s.solution(ss));
+		System.out.println(s.solution(123));
 	}
 }
