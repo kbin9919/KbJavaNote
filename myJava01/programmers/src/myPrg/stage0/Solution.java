fb117e3717f0c20e8fcc73d427cb5913d589a181
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-03-20 09:01:42 +0900

    프로그래머스 12916번 문제
    문자열 내 p와 y의 개수

--- myJava01/programmers/src/myPrg/stage0/Solution.java
+++ myJava01/programmers/src/myPrg/stage0/Solution.java
@@ -1,22 +1,26 @@
 package chap0;
 
 public class Solution {
-	
-	public int solution(int n) {
-        int answer = 0;
-        String str = String.valueOf(n); //"123"
-        int[] x = new int[str.length()]; //길이가 문자열의 길이와 같음 : x[3]
-        String[] s = new String[str.length()]; //길이가 문자열의 길이와 같음 : s[3]
-        for(int i = 0; i <str.length(); i++) { //0부터 2까지
-        	s[i] = String.valueOf(str.charAt(i)); 
-        	x[i] = Integer.parseInt(s[i]); //x[0] = 1, x[1] = 2, x[2] = 3
-        	System.out.println(x[i]);
-        	answer += x[i]; //0 + 1 + 2 + 3;
-        }
-        return answer;
-    }
+
+	boolean solution(String s) {
+		boolean answer = true;
+		int num1 = 0;
+		int num2 = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+				num1++;
+			} else if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+				num2++;
+			}
+		}
+		if(num1 != num2) {
+			return answer = false;
+		}
+		return answer;
+	}
+
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		System.out.println(s.solution(123));
+		System.out.println();
 	}
 }
