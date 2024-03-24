c0d7b5212011e76e35c484d1b47c4460b7d160b1
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-03-24 23:05:37 +0900

    프로그래머스 12933, 12947, 76501, 12919번 문제
    정수 내림차순으로 배치하기
    하샤드 수
    음양 더하기
    서울에서 김서방 찾기

---
+++ myJava01/programmers/src/myPrg/stage0/Solution08.java
@@ -0,0 +1,14 @@
+package programmers.stage1;
+
+public class Solution08 {
+	public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i<absolutes.length; i++) {
+        	if(signs[i] == false) {
+        		absolutes[i] = -absolutes[i];
+        	}
+        	answer += absolutes[i];
+        }
+        return answer;
+    }
+}
