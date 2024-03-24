d3cdb135e33eff1fb4c1396b486507c86580a731
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-03-24 23:09:25 +0900

    프로그래머스 12933번 문제
    정수 내림차순으로 배치하기

---
+++ myJava01/programmers/src/myPrg/stage0/Solution06.java
@@ -0,0 +1,28 @@
+package programmers.stage1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution06 {
+	 public long solution(long n) {
+	        long answer = 0;
+	        String str = Long.toString(n);
+	        Long[] x = new Long[str.length()];
+	        List<Long> y = new ArrayList<Long>();
+	        String st = " ";
+	        for(int i =0; i<str.length(); i++) {
+	        	x[i] = Long.valueOf(str.charAt(i)-48);
+	        	y.add(x[i]);
+	        }
+	        y.sort(null);
+	        for(int i = y.size()-1; i>=0; i--) {
+	        	st += y.get(i);
+	        }
+	        return answer = Long.parseLong(st.substring(1, st.length()));
+	    }
+	 public static void main(String[] args) {
+		 Solution06 s = new Solution06();
+		 s.solution(118372);
+		 System.out.println(s.solution(118372));
+	}
+}
