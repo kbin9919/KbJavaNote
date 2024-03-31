94df66a5ae2ab4150e5ca78000e2bdfbb6d6d954
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-03-31 18:55:04 +0900

    프로그래머스 12917번 문제
    문자열 내림차순으로 배치하기

---
+++ myJava01/programmers/src/myPrg/stage0/Solution19.java
@@ -0,0 +1,22 @@
+package programmers.stage1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution19 {
+	public String solution(String s) {
+		String str = s;
+		String answer = "";
+		ArrayList<Character> x = new ArrayList<>();
+		for (int i = 0; i < str.length(); i++) {
+			x.add(str.charAt(i));  
+		}
+		Collections.sort(x, Collections.reverseOrder());
+		x.sort(null);
+		Collections.reverse(x);
+		for (int i = 0; i < x.size(); i++) {
+			answer += x.get(i);
+		}
+		return answer;
+	}
+}
