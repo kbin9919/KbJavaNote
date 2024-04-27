591f1de9b3cc31f4a435735a4020ced08f71f82a
kbin9919 <160497040+kbin9919@users.noreply.github.com>
2024-04-27 16:00:32 +0900

    프로그래머스 132267번 문제
    콜라 문제

---
+++ Solution40.java
@@ -0,0 +1,23 @@
+package programmers.stage1;
+
+public class Solution40 {
+	public int solution(int a, int b, int n) {
+
+		int answer = 0;
+		while (true) {
+			// 남은 병 수
+			int pN = n % a;
+			// 반환받을 병 수
+			int rC1 = n / a * b;
+
+			answer += rC1;
+			n = rC1 + pN;
+
+			System.out.println("남은 빈 병 수 : " + n);
+			if (n < a) {
+				break;
+			}
+		}
+		return answer;
+	}
+}
