21ee075deaca30f148c0061a0ca8f7b95ab00b5e
DESKTOP-AK7R8BR\USER <kbin9919@naver.com>
2024-04-28 16:25:25 +0900

    JSP

---
+++ servletWorkspace/prj03/src/main/java/prj03/Test.java
@@ -0,0 +1,17 @@
+package prj03;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class Test extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
+	}
+}
