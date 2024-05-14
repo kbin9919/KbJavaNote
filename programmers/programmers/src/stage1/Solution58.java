8e5bd310dd0dcd1b22eb19f96927d0cfb8ea05f5
DESKTOP-AK7R8BR\USER <kbin9919@naver.com>
2024-05-14 12:23:20 +0900

    프로그래머스 1단계

---
+++ programmers/programmers/src/stage1/Solution58.java
@@ -0,0 +1,31 @@
+package stage1;
+
+public class Solution58 {
+	public int solution(String[] babbling) {
+		int answer = 0;
+
+		String[] str = { "aya", "ye", "woo", "ma" };
+
+		for (String b : babbling) {
+			for (String s : str) {
+				if(b.indexOf(s + s) >= 0) {
+					break;
+				}
+				b = b.replaceAll(s, " ");
+			}
+			if(b.trim().length() == 0) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String str[] = { "aya", "yee", "u", "maa" };
+		String str2[] = { "ayaye", "uuuye", "yeye", "yema woo", "ayaayaa" };
+		Solution58 s = new Solution58();
+		s.solution(str2);
+		String str3 = "woo";
+		System.out.println(str3.replaceAll("woo", "") + "1");
+	}
+}
