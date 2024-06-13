54d64143b406ddca0fe68bf901567b90f24cbcac
DESKTOP-AK7R8BR\USER <kbin9919@naver.com>
2024-06-13 10:39:40 +0900

    프로그래머스 1단계

--- programmers/programmers/src/stage1/Solution77.java
+++ programmers/programmers/src/stage1/Solution77.java
@@ -7,25 +7,20 @@ public class Solution77 {
 		String[] answer = new String[players.length];
 		HashMap<String, Integer> PlayerMap = new HashMap<>();
 		HashMap<Integer, String> RankMap = new HashMap<>();
-
 		for (int i = 0; i < players.length; i++) {
 			PlayerMap.put(players[i], i);
 			RankMap.put(i, players[i]);
 		}
 		for (int i = 0; i < callings.length; i++) {
-
 			// 추월한 유저 순위
 			// 추월한 유저 이름
 			int currentRank = PlayerMap.get(callings[i]);
 			String player = RankMap.get(currentRank);
-
 			// 바로 앞 플레이어
 			String frontPlayer = RankMap.get(currentRank - 1);
-
 			// swap
 			PlayerMap.put(player, currentRank - 1);
 			PlayerMap.put(frontPlayer, currentRank);
-
 			RankMap.put(currentRank - 1, player);
 			RankMap.put(currentRank, frontPlayer);
 		}
