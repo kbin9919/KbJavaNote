25fbe91f20442c18b3b909181de5d95a14c50058
DESKTOP-AK7R8BR\USER <kbin9919@naver.com>
2024-06-23 20:33:52 +0900

    프로그래머스 2단계 문제

---
+++ programmers/programmers/src/stage2/Solution05.java
@@ -0,0 +1,20 @@
+package stage2;
+
+public class Solution05 {
+	public int[] solution(String s) {
+		int removeCnt = 0;
+		int changeCnt = 0;
+		while (!s.equals("1")) {
+			int sl = s.length();
+			s = s.replace("0", "");
+			int newSl = s.length();
+			removeCnt += (sl - newSl);
+			
+			s = Integer.toBinaryString(newSl);
+			changeCnt++;
+		}
+		int[] answer = {changeCnt,removeCnt};
+		
+		return answer;
+	}
+}
