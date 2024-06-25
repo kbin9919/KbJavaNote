07be6c80d1cf4f8fd5febe5e7cb2ffbddb919469
DESKTOP-AK7R8BR\USER <kbin9919@naver.com>
2024-06-25 14:23:52 +0900

    사용자 어노테이션 생성, @Annotation 예외처리

---
+++ springBootWorkspace/app01/src/main/java/com/kh/app01/advice/KhControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.kh.app01.advice;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(basePackages = "com.kh.app01")
+public class KhControllerAdvice {
+
+    @ExceptionHandler(NullPointerException.class)
+    public void m01(Model model){
+        System.out.println("KhControllerAdvice.m01");
+    }
+    @ExceptionHandler(ArithmeticException.class)
+    public void m02(){
+        System.out.println("KhControllerAdvice.m02");
+    }
+    @ExceptionHandler(Exception.class)
+    public void m03(){
+        System.out.println("KhControllerAdvice.m03");
+    }
+}
