2b39cdc59e3366dc894c244b458a3981112f5c5c
DESKTOP-AK7R8BR\USER <kbin9919@naver.com>
2024-07-25 17:04:22 +0900

    프로그래머스 2단계

---
+++ programmers/programmers/src/stage2/Solution07.java
@@ -0,0 +1,29 @@
+package stage2;
+
+public class Solution07 {
+	public int solution(int n) {
+		String binaryN = Integer.toBinaryString(n);
+		int binaryNCnt = countChar(binaryN, '1');
+		
+		while(true) {
+			n++;
+			binaryN = Integer.toBinaryString(n);
+			int newBinaryNCnt = countChar(binaryN, '1');
+			if(binaryNCnt == newBinaryNCnt) {
+				break;
+			}
+		}
+        int answer = 0;
+        return answer;
+    }
+
+	public static int countChar(String str, char targetChar) {
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == targetChar) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
